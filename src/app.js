1cd2f21a64577221ab0985233263f161bb13ae0e
lobotryaskina <sofiaryaskina@gmail.com>
2023-08-03 22:15:00 +0300

    delete app js

--- src/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Count the sum of scores from an object
- * @param {Array} scores
- * @example - getScore({ Ann: 5, Joe: 10 }) // 15
- */
-export const getScore = (scores) => {
-  return Object.values(scores).reduce((sum, score) => sum + score, 0);
-}
